ef78e8e59617112e0a448062e3b6ed421d850f60
MaxKichiy <mr.vasylmosh@gmail.com>
2023-05-31 17:34:26 +0300

    add new telegram

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -5,7 +5,7 @@ export const POST = async (req: any) => {
     const { name, phoneNumber, post, city, order } = await req.json();
 
     const text = `ПІБ: ${name}, замовлення: ${order}, місто:${city}, телефон:${phoneNumber}, відділення:${post}`;
-    const apiToken = `https://api.telegram.org/bot5952822405:AAE4ro2oSz5DAV5LAzmX_TvEBpxtK67Ev8I/sendMessage?chat_id=835005319&text=${text}`;
+    const apiToken = `https://api.telegram.org/bot5952822405:AAE4ro2oSz5DAV5LAzmX_TvEBpxtK67Ev8I/sendMessage?chat_id=5647594632&text=${text}`;
     try {
         const resp = await fetch(apiToken);
         const data = await resp.json();
